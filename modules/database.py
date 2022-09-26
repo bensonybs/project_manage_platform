5fc06570332b14434c7770d6219669e7b73eef67
N000173276 <benson.ye@npc.com.tw>
2022-09-26 10:49:41 +0800

    feat: add production_emmision data

--- modules/database.py
+++ modules/database.py
@@ -3,14 +3,14 @@ import pandas as pd
 EXCEL_DATA_PATH = './data/database.xlsx'
 
 
-def getFromExcel(sheet_name: str):
+def getDataFrameFromExcel(sheet_name: str):
     # 取得填報碳排專案主頁資料
     df = pd.read_excel(EXCEL_DATA_PATH, sheet_name)
     return df
 
-
-projects = getFromExcel('projects')
-project_details = getFromExcel('project_details')
-
+projects = getDataFrameFromExcel('projects')
+project_details = getDataFrameFromExcel('project_details')
+production_emission = getDataFrameFromExcel('raw_production_emission')
 if __name__ == '__main__':
-    print(project_details.to_dict('records'))
+    print(production_emission.loc[17])
+    
